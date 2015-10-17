3e95e5ff7c883701a9908ca2a436a4f0d9823ca3
Charles <codprestige10@gmail.com>
2015-10-17 13:46:16 -0400

    so far this code will get contributor names and all the adds for the week, I'm having a hard time getting the adds per contributor though

--- lib/commitchamp.rb
+++ lib/commitchamp.rb
@@ -7,6 +7,19 @@ require "commitchamp/version"
 module Commitchamp
   class App
     def initialize
+    	@search = PullRequests.new
+    	puts "Welcome to CommitChamp!"
+    	puts "Please make a choice"
+    	puts "'lc' for a list of contributors"
+    	puts "'a' for number of additions"
+    	choice = gets.chomp.downcase
+    	if choice == "lc"
+    		@search.get_authors
+    	elsif choice == "a"
+    		@search.get_commits
+    	else
+    		puts "Good bye"
+    	end
     end
 
     def run
@@ -14,6 +27,64 @@ module Commitchamp
     end
   end
 end
+
+module Commitchamp
+	class PullRequests
+		include HTTParty
+		base_uri "https://api.github.com"
+		def initialize
+			puts "What is your authorization token?"
+			auth_token = gets.chomp
+			@auth = {
+				"Authorization" => "token #{auth_token}",
+				"User-Agent"    => "HTTParty"
+			}
+		end
+
+		def get_authors
+			puts "Which organization do you want to search?"
+			owner = gets.chomp.downcase
+			puts "Which repository do you want to look into?"
+			repo = gets.chomp.downcase
+			puts "CONTRIBUTORS"
+		    names = self.get_data(owner, repo)
+		    names.each { |x| puts x["author"]["login"] }
+			binding.pry
+		end
+
+		def get_data(owner, repo)
+			PullRequests.get("/repos/#{owner}/#{repo}/stats/contributors", :headers => @auth)
+		end
+
+		def get_commits
+			puts "Who is the owner?"
+			owner = gets.chomp.downcase
+			puts "Which repo?"
+			repo = gets.chomp.downcase
+			response = self.get_data(owner, repo)
+			total = response.each {|y| y["total"]}
+			commits = response.each do |x| week = x["weeks"]
+									 ads = week.each do |y| puts y["a"] 
+										end
+									end 
+			
+		end
+
+		
+
+	end
+end
+
+
+
+
+
+
+
+
+
+
+
 
 app = Commitchamp::App.new
 app.run
